187cf917c4fb575b5ec2eb92802b5595a85d63cc
p33yush <peeyushhanumante@gmail.com>
2026-09-21 16:05:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Largest/kth-largest.cpp
+++ Difficulty: Medium/Kth Largest/kth-largest.cpp
@@ -6,7 +6,7 @@ class Solution {
         for(int i:arr){
             pq.push(i);
         }
-        int ans=0;
+        int ans=-1;
         while(!pq.empty() && k!=0){
             ans=pq.top();
             pq.pop();
