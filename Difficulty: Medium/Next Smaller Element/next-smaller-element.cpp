bdac98e5dcef52051d42498b71a82eaed88f015b
p33yush <peeyushhanumante@gmail.com>
2026-09-24 00:06:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Smaller Element/next-smaller-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<int> nextSmallerEle(vector<int>& arr) {
+        //  code here
+        int n = arr.size();
+        vector<int> res(n,-1);
+        stack<int> st;
+        
+        for(int i = n-1;i>=0;i--){
+            while(!st.empty() && st.top()>=arr[i]){
+                st.pop();
+            }
+            if(!st.empty()) res[i] = st.top();
+            st.push(arr[i]);
+        }
+        
+        return res;
+    }
+};
